edb8531871cf09ef2e03d6809868095595fb5a3f
mmagnuski <mmagnuski@swps.edu.pl>
2018-07-27 14:11:50 +0200

    FIX: fix letters import

--- borsar/utils.py
+++ borsar/utils.py
@@ -137,7 +137,7 @@ def valid_windows(raw, tmin=None, tmax=None, winlen=2., step=1.):
 
 
 def create_fake_raw(n_channels=4, n_samples=100, sfreq=125.):
-    from str import letters
+    from string import ascii_letters
     ch_names = list(letters[:n_channels])
     data = np.zeros((n_channels, n_samples))
     info = mne.create_info(ch_names, sfreq, ch_types='eeg')
