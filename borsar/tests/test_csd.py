3adca863408a458318876dd0b60836993b9d2ccd
mmagnuski <mmagnuski@swps.edu.pl>
2018-08-02 01:39:15 +0200

    FIX: slightly higher tolerance

--- borsar/tests/test_csd.py
+++ borsar/tests/test_csd.py
@@ -26,7 +26,7 @@ def test_csd_base():
 
     # make sure erp_csd and erp_csd_py are the same
     # (use lower rtol for a stricter test)
-    assert_allclose(erp_csd, erp_csd_py, rtol=3e-9)
+    assert_allclose(erp_csd, erp_csd_py, rtol=5e-9)
 
     # make sure G and H were not changed during computation
     assert (G == G_orig).all()
