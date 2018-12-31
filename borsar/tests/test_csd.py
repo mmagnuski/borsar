63335cbb32f48268325f520661f00a825ad29682
mmagnuski <mmagnuski@swps.edu.pl>
2018-12-31 17:19:13 +0100

    FIX: fix tests

--- borsar/tests/test_csd.py
+++ borsar/tests/test_csd.py
@@ -45,7 +45,7 @@ def test_csd_mne():
     # check that is works for epochs too:
     events = np.zeros((4, 3), dtype='int')
     events[:, -1] = 1
-    events[:, 0] = [10, 50, 110]
+    events[:, 0] = [10, 50, 110, 165]
     epochs = mne.Epochs(raw, events, event_id=1, tmin=0., tmax=0.15,
                         baseline=None)
     epochs_csd = current_source_density(epochs.copy(), G, H)
