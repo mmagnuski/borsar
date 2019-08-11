0dcbec9587ed67200d3e7d52dabbcadb98d1c4de
mmagnuski <mmagnuski@swps.edu.pl>
2019-08-11 13:56:43 +0200

    FIX: fix test and fix scikit-image installation

--- borsar/tests/test_project.py
+++ borsar/tests/test_project.py
@@ -51,11 +51,11 @@ def test_paths():
 
     # use relative_to=some_name
     pth.add_path('rodżer', 'another', relative_to='test2')
-    expected_path = Path(r'temp\std1\abc\another')
+    expected_path = Path('temp/std1/abc/another')
     assert pth._get_path('rodżer', 'study1', '') == expected_path
 
     pth.add_path('rodżer2', 'another', task='task1', relative_to='test2')
-    expected_path = Path(r'temp\std1\tsk1\another')
+    expected_path = Path('temp/std1/tsk1/another')
     assert pth._get_path('rodżer2', 'study1', 'task1') == expected_path
 
     msg = 'Task "task1" has been already registered for study "study1".'
