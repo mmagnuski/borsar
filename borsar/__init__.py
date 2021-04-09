8ccf598617617cf2b5db535e534b016b3819e8f1
mmagnuski <mmagnuski@swps.edu.pl>
2021-04-09 17:56:33 +0200

    ENH: better init

--- borsar/__init__.py
+++ borsar/__init__.py
@@ -1,1 +1,4 @@
-from . import (channels, csd, freq, project, stats, utils, viz)
+from borsar import (channels, cluster, csd, freq, project, stats, utils, viz)
+
+from borsar.cluster import Clusters, find_clusters, read_cluster
+from borsar.utils import find_index, find_range, write_info, read_info
