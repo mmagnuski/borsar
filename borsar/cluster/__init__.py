85f336d492156e72f524150a0b74f7708ccfdf3d
mmagnuski <mmagnuski@swps.edu.pl>
2023-10-05 11:47:41 +0200

    init change

--- borsar/cluster/__init__.py
+++ borsar/cluster/__init__.py
@@ -1,4 +1,4 @@
 from .obj import Clusters, read_cluster
 from .label import find_clusters
-from .stats import cluster_based_regression
+from .stats import cluster_based_regression, permutation_cluster_test_array
 from .utils import construct_adjacency_matrix
