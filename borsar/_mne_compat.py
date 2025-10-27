e891ab98352bda15ccf246e640e182d370cce958
mmagnuski <mmagnuski@swps.edu.pl>
2025-10-27 22:37:46 +0100

    drop some old mne causing conflicts with new matplotlib

--- borsar/_mne_compat.py
+++ borsar/_mne_compat.py
@@ -4,8 +4,7 @@ from mne.viz.topomap import _plot_topomap
 
 from packaging import version
 mne_version = version.parse(mne.__version__)
-has_1_1 = mne_version >= version.parse('1.1.dev0')
-has_1_2 = mne_version >= version.parse('1.2.dev0')
+has_1_4 = mne_version >= version.parse('1.4.dev0')
 
 _BORDER_DEFAULT = 'mean'
 _EXTRAPOLATE_DEFAULT = 'box'
@@ -15,20 +14,19 @@ _EXTRAPOLATE_DEFAULT = 'box'
 #                                present in docdict)
 @fill_doc
 def plot_topomap(data, pos, vmin=None, vmax=None, cmap=None, sensors=True,
-                 res=64, axes=None, names=None, show_names=False, mask=None,
+                 res=64, axes=None, names=None, mask=None,
                  mask_params=None, outlines='head', contours=6,
-                 image_interp='bilinear', show=True, onselect=None,
+                 image_interp='cubic', show=True, onselect=None,
                  extrapolate=_EXTRAPOLATE_DEFAULT, sphere=None,
                  border=_BORDER_DEFAULT, ch_type='eeg', cnorm=None):
     """Plot a topographic map as image.
     (docs as in mne)
     """
     sphere = _check_sphere(sphere)
-    if has_1_2 or has_1_1:
-        if image_interp == 'bilinear':
-            image_interp = 'cubic'
+    if image_interp == 'bilinear':
+        image_interp = 'cubic'
 
-    if has_1_2:
+    if has_1_4:
         return _plot_topomap(
             data, pos, vmin=vmin, vmax=vmax, cmap=cmap, sensors=sensors,
             res=res, axes=axes, names=names, mask=mask,
@@ -36,11 +34,6 @@ def plot_topomap(data, pos, vmin=None, vmax=None, cmap=None, sensors=True,
             image_interp=image_interp, show=show, onselect=onselect,
             extrapolate=extrapolate, sphere=sphere, border=border,
             ch_type=ch_type, cnorm=cnorm)
-    elif has_1_1:
-        return _plot_topomap(data, pos, vmin, vmax, cmap, sensors, res, axes,
-                             names, show_names, mask, mask_params, outlines,
-                             contours, image_interp, show, onselect,
-                             extrapolate, sphere=sphere, border=border,
-                             ch_type=ch_type)
     else:
-        raise RuntimeError('You need to have MNE-Python version >= 1.1.0')
+        raise RuntimeError('You need to have MNE-Python version >= 1.4.0')
+
