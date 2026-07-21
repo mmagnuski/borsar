23a23cb95dfababa54487cea2de7c57e15f86e3f
Mikolaj Magnuski <mmagnuski@swps.edu.pl>
2026-07-21 22:36:41 +0200

    clean up codex overparamterized mess, make it more readable

--- borsar/tests/test_viz.py
+++ borsar/tests/test_viz.py
@@ -307,32 +307,38 @@ def test_heatmap_uses_xarray_coords():
 
 
 @pytest.mark.skipif(not has_xarray(), reason="requires xarray")
-@pytest.mark.parametrize(
-    ('coords', 'x_axis', 'y_axis', 'expected_extent'),
-    [
-        ({}, None, None, [-0.5, 3.5, -0.5, 2.5]),
-        ({'time': [10., 20., 30., 40.]}, None, None,
-         [5., 45., -0.5, 2.5]),
-        ({'frequency': [8., 10., 12.], 'time': [0.1, 0.2, 0.3, 0.4]},
-         np.array([1., 3., 5., 7.]), np.array([20., 21., 22.]),
-         [0., 8., 19.5, 22.5]),
-    ]
-)
-def test_heatmap_xarray_incomplete_coords(coords, x_axis, y_axis,
-                                          expected_extent):
-    """Use defaults for missing xarray coordinates and honor explicit axes."""
+def test_heatmap_xarray_incomplete_coords():
+    """Use available xarray coordinates unless explicit axes are passed."""
     import xarray as xr
 
     xarr = xr.DataArray(
         np.arange(12, dtype='float').reshape(3, 4),
-        dims=('frequency', 'time'), coords=coords
+        dims=('frequency', 'time')
     )
 
-    ax = heatmap(xarr, x_axis=x_axis, y_axis=y_axis, colorbar=False)
+    ax = heatmap(xarr, colorbar=False)
 
-    np.testing.assert_allclose(ax.images[0].get_extent(), expected_extent)
+    np.testing.assert_allclose(ax.images[0].get_extent(),
+                               [-0.5, 3.5, -0.5, 2.5])
     assert ax.get_xlabel() == 'time'
     assert ax.get_ylabel() == 'frequency'
+    plt.close(ax.figure)
+
+    xarr = xarr.assign_coords(time=[10., 20., 30., 40.])
+
+    ax = heatmap(xarr, colorbar=False)
+
+    np.testing.assert_allclose(ax.images[0].get_extent(),
+                               [5., 45., -0.5, 2.5])
+    plt.close(ax.figure)
+
+    xarr = xarr.assign_coords(frequency=[8., 10., 12.])
+
+    ax = heatmap(xarr, x_axis=np.array([1., 3., 5., 7.]),
+                 y_axis=np.array([20., 21., 22.]), colorbar=False)
+
+    np.testing.assert_allclose(ax.images[0].get_extent(),
+                               [0., 8., 19.5, 22.5])
     plt.close(ax.figure)
 
 
